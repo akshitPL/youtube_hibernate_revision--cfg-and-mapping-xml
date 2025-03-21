57ff70ad72b51572653f7a69ac094c76b651c812
akshitPL <147038979+akshitPL@users.noreply.github.com>
2025-03-21 17:16:51 +0530

    update

--- src/main/java/com/tut/youtube_hibernate_revision/App.java
+++ src/main/java/com/tut/youtube_hibernate_revision/App.java
@@ -1,7 +1,12 @@
 package com.tut.youtube_hibernate_revision;
 
 import org.hibernate.cfg.Configuration;
+
+import com.tut.modal.entity.Employee;
+
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 /**
  * Hello world!
  *
@@ -9,8 +14,15 @@ import org.hibernate.SessionFactory;
 public class App 
 {
     public static void main( String[] args ) {
+    	
+    	Employee e= new Employee(2,"akshit","male",96523);
+    	
     SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+    Session session = sessionFactory.openSession();
+     Transaction tx= session.beginTransaction();
     
-    System.out.println(sessionFactory);
+    session.save(e);
+    
+   tx.commit();
 }
 }
